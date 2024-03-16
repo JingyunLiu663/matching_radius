8bdd8bb546b60dff46bb77c764faa035a762cc87
jingyunliu663 <jingyunliu007@gmail.com>
2024-03-16 16:13:25 +0800

    delete test.py add train_test.py

--- train_test.py
+++ train_test.py
@@ -4,10 +4,8 @@ import os
 from rl_agents.dqn import DqnAgent
 from rl_agents.ddqn import DDqnAgent
 from rl_agents.dueling_dqn import DuelingDqnAgent
-from log_generation import ActionTracker
 from config import *
 from utilities import *
-from utils.draw_snapshots import draw_simulation
 
 def update_simulator_args(simulator, args):
     '''
@@ -211,3 +209,22 @@ def simulation_greedy(simulator, test_num, logger):
                 simulator.step()
             end_time = time.time()
             logger.info(f"epoch : {num} == total_reward:{simulator.total_reward},matching_rate:{simulator.matched_requests_num/simulator.total_request_num},total_request_num:{simulator.total_request_num},matched_request_num:{simulator.matched_requests_num},occupancy_rate:{simulator.occupancy_rate},occupancy_rate_no_pickup:{simulator.occupancy_rate_no_pickup},wait_time:{simulator.waiting_time / simulator.matched_requests_num},pickup_time:{simulator.pickup_time / simulator.matched_requests_num}")
+
+def simulation_test(args, agent, simulator, logger, test_num=10):
+
+    for num in range(test_num):
+        print('test round: ', num)
+
+        for date in TEST_DATE_LIST:
+            simulator.experiment_date = date
+            simulator.reset()
+
+            for _ in range(simulator.finish_run_step):
+                if simulator.wait_requests.shape[0] > 0:
+                    states = get_states(simulator)
+                    action_indices, matching_radius = get_actions_given_states(agent, states, args)
+                    simulator.wait_requests['action_index'] = action_indices
+                    simulator.wait_requests['matching_radius'] = matching_radius
+                simulator.step()  # observe the transition
+
+        logger.info(f"epoch : {num} == total_reward:{simulator.total_reward},matching_rate:{simulator.matched_requests_num/simulator.total_request_num},total_request_num:{simulator.total_request_num},matched_request_num:{simulator.matched_requests_num},occupancy_rate:{simulator.occupancy_rate},occupancy_rate_no_pickup:{simulator.occupancy_rate_no_pickup},wait_time:{simulator.waiting_time / simulator.matched_requests_num},pickup_time:{simulator.pickup_time / simulator.matched_requests_num}")
